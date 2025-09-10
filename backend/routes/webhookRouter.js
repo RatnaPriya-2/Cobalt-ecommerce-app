090891cfcb91e58e00338c744458c76d3cc57bcd
RatnaPriya-2 <ratnapriya.t@gmail.com>
2025-09-10 18:39:06 +0530

    k

--- backend/routes/webhookRouter.js
+++ backend/routes/webhookRouter.js
@@ -1,13 +1,12 @@
 import express from "express";
 
-import bodyParser from "body-parser";
 import StripeWebhookController from "../controllers/stripeWebhookController.js";
 
 const webHookRouter = express.Router();
 
 webHookRouter.post(
   "/",
-  bodyParser.raw({ type: "application/json" }),
+  express.raw({ type: "application/json" }),
   StripeWebhookController
 );
 
